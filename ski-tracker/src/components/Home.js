30cc211b313e565cf7930a074e0f0536450b611c
Konrad Lach <93342886+KonradLach@users.noreply.github.com>
2022-10-09 10:16:15 -0700

    Add files via upload

--- ski-tracker/src/components/Home.js
+++ ski-tracker/src/components/Home.js
@@ -1,70 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css';
 import Skiresortcard from './Skiresortcard';
-import ReactWeather, { useOpenWeather } from 'react-open-weather';
 
-function Home({apiData}) {
+function Home({apiData,skiData,lat,long}) {
 
-    const [skiData,setSkiData] = useState([])
-    const [lat, setLat] = useState([]);
-    const [long, setLong] = useState([]);
-    const [skiDataLoading,setSkiDataLoading] = useState(true)
-    //fetches ski resort data from skimap.org
-    let id = [160,165,162,175]
-
-    function getResortFromApiAsync(id) {
-      const promises = []
-      for(let i = 0; i<id.length;i++){
-        promises.push(fetch(`https://skimap.org/SkiAreas/view/${id[i]}.json`))
-      }
-      Promise.all(promises)
-        .then(([res1,res2,res3,res4])=>Promise.all([res1.json(),res2.json(),res3.json(),res4.json()])) 
-        .then(([data1,data2,data3,data4]) => {
-          console.log(data1)
-          setSkiData([...skiData,{name:data1.name, latitude:data1.latitude,longitude:data1.longitude},{name:data2.name, latitude:data2.latitude,longitude:data2.longitude},{name:data3.name, latitude:data3.latitude,longitude:data3.longitude},{name:data4.name, latitude:data4.latitude,longitude:data4.longitude}])
-          setSkiDataLoading(false)
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-     }
-
-
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          setLat(position.coords.latitude);
-          setLong(position.coords.longitude);
-        });
-
-        console.log("Latitude is:", {lat})
-        console.log("Longitude is:", {long})
-      }, []);
-
-
-
-      useEffect(() => {
-          getResortFromApiAsync(id)
-      },[])
-  
-      const renderResortCards = () => {
-        const cards = skiData.map((resort) =>
-        <Skiresortcard resort = {resort} key={resort.name}/>)
-        return(
-          <div className='skiresortcontainer grid grid-cols-4'>
-            {cards}
-          </div>)
-      }
-
-  if(skiDataLoading){
-    return <div className='App'>Loading..</div>
-  }
 
   return (
     <div className="Home">
         <h1 className='text-center mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>Ski-Tracker</h1>
         <h3 className='text-center mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400'>My current postion. Latitude: {lat} Longitude: {long} </h3>
-          {renderResortCards()}
-          {/* {apiData.main.temp} */}
+        <div className='skiresortcontainer grid grid-cols-4'>
+          <Skiresortcard resort={skiData[0]} temp={apiData[1]} description={apiData[0]} snow={apiData[2]}/>
+          <Skiresortcard resort={skiData[1]} temp={apiData[4]} description={apiData[3]} snow={apiData[5]}/>
+          <Skiresortcard resort={skiData[2]} temp={apiData[7]} description={apiData[6]} snow={apiData[8]}/>
+          <Skiresortcard resort={skiData[3]} temp={apiData[10]} description={apiData[9]} snow={apiData[11]}/>
+
+          </div>
     </div>
   );
 }
--- ski-tracker/src/components/Skiresortcard.js
+++ ski-tracker/src/components/Skiresortcard.js
@@ -1,11 +1,13 @@
 import "../style/Skiresortcard.css"
 import React from "react";
-function Skiresortcard({resort}) {
+function Skiresortcard({resort,temp,description,snow}) {
   console.log(resort.name)
     const MY_ACCESS_KEY = "7ZRNVRYDAj2LhFsIzZYj4jaGhns6RB8-b2UEdgAfUBM"
+    let website = "#"
+    console.log(resort.website)
   return (
       <div className="skiresort">
-        <a href="#" className="group relative block bg-black">
+        <a href={resort.website} className="group relative block bg-black">
             <img
                 alt={resort.name}
                 src={"https://images.unsplash.com/photo-1600332303625-c7d287a9f4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"}
@@ -14,9 +16,11 @@ function Skiresortcard({resort}) {
 
             <div className="relative p-8">
                 <p className="text-sm font-medium uppercase tracking-widest text-blue-800">
-                Temp: 72F Snowy 
+                Temp: {temp}F {description}
                 </p>
-
+                <p className="text-sm font-small uppercase tracking-widest text-blue-800">
+                {snow} inch snow in last 3 hours
+                </p>
                 <p className="text-2xl font-bold text-white">{resort.name}</p>
 
                 <div className="mt-64">
